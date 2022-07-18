83179a304c5f074583eacbcfd9100c8df12a6106
Georgi Angelov <106385924+GeorgiAngelov98@users.noreply.github.com>
2022-07-18 20:33:22 +0300

    Add files via upload

---
+++ For Loop - Lab/06. Vowels Sum.js
@@ -0,0 +1,21 @@
+function vowelsSum(input){
+    let text = input[0];
+    let sum = 0;
+
+    for (let i = 0; i < text.length; i++){
+        let letter = text[i]
+        switch (letter){
+            case "a" : sum += 1;break;
+            case "e": sum += 2;break;
+            case "i": sum += 3;break;
+            case "o": sum += 4;break;
+            case "u": sum += 5;break;
+        }
+    }
+    console.log(sum)
+
+}
+vowelsSum(["hello"])
+
+// буква	a	e	i	o	u
+// стойност	1	2	3	4	5
---
+++ For Loop - Lab/08. Numbers, Divisible by 9.js
@@ -0,0 +1,15 @@
+function numbersDivisibleBy9(input){
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+    let sum = 0; 
+    let numb = ""
+    for (let i = start; i <= end; i++){
+       if ( i % 9 === 0){
+           sum += i
+           numb += i + "\n"
+       }
+    }
+    console.log(`The sum: ${sum}`)
+    console.log(numb)
+}
+numbersDivisibleBy9(["100", "200"])
